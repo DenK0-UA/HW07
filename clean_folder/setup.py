15e610e9bf650b958f2d400218af6c4745bf0cc0
DenK0 <denidinamo@gmail.com>
2023-11-30 01:46:56 +0200

    6

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -5,7 +5,7 @@ setup(name='clean_folder',
       description='This code is a Python script that organizes files in a specified folder.'
                   'It normalizes file names, moves them to respective categories (images, videos, documents, audio, archives), and removes empty folders.'
                   'Unknown files are moved to a separate folder.',
-      url='https://github.com/DenK0-UA/Python_Core_Hw07/tree/master/clean_folder/',
+      url='https://github.com/DenK0-UA/Python_Core_Hw07/',
       author='Denis Yevtushenko',
       author_email='denidinamo@gmail.com',
       license='MIT',
