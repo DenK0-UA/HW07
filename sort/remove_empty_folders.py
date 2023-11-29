0fc6e96d6c0720e659d50ba1c745474fe0ad222b
DenK0 <denidinamo@gmail.com>
2023-11-30 01:50:04 +0200

    7

--- sort/remove_empty_folders.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def remove_empty_folders(folder):
-    for root, dirs, files in os.walk(folder, topdown=False):
-        for i in dirs:
-            dir_path = os.path.join(root, i)
-            if not os.listdir(dir_path):
-                os.rmdir(dir_path)
